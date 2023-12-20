1eaa138f41b99301c527c71b626cd8b11bb5a937
Ccottin <80279269+Ccottin@users.noreply.github.com>
2023-12-20 19:18:46 +0100

    Trying to get out of midBronze - added monsters dodge but isnt doing great

--- 00.ts
+++ 00.ts
@@ -17,6 +17,8 @@ type drone = {
     battery: number;
     holdScanned: number[];
     radarBlip: radar[];
+    droneVisibility: number;
+    visibleMonsters: Map<number, creature>;
     firstMove: boolean;
     goSurface: boolean;
 }
@@ -42,6 +44,8 @@ type tuple = {
 type fixedValues = {
     firstMove: boolean;
     goSurface: boolean;
+    countLight: number;
+    droneVisibility: number;
 }
 
 const readLine = require('readline');
@@ -55,8 +59,8 @@ const rl = readLine.createInterface({
 
 const creatureCount: number = parseInt(readline());
 
-let creatures: creature[] = [];
-let monsters: number[] = [];
+let fishies = new Map<number, creature>();
+let monsters = new Map<number, creature>();
 
 for (let i = 0; i < creatureCount; i++) {
     let inputs: string[] = readline().split(' ');
@@ -66,9 +70,10 @@ for (let i = 0; i < creatureCount; i++) {
         type: parseInt(inputs[2]),
         isVisible: false,
     }
-    creatures.push(newCreature);
     if (newCreature.type == -1)
-        monsters.push(newCreature.creatureId);
+        monsters.set(newCreature.creatureId, newCreature);
+    else
+        fishies.set(newCreature.creatureId, newCreature);
 }
 
 let player: player = { drones: [], creatureScanned: [] };
@@ -76,14 +81,21 @@ let foe: player = { drones: [], creatureScanned: [] };
 let fixedValues: fixedValues[] = [{
     firstMove: true,
     goSurface: false,
+    countLight: 4,
+    droneVisibility: 800,
 }, {
     firstMove: true,
     goSurface: false,
-    }];
+    countLight: 4,
+    droneVisibility: 800,
+}];
 
 /*                 game loop					*/
 
 while (true) {
+    monsters.forEach((elem) => { elem.isVisible = false; });
+    fishies.forEach((elem) => { elem.isVisible = false; });
+
     player.score = parseInt(readline());
     foe.score = parseInt(readline());
 
@@ -109,7 +121,9 @@ while (true) {
             holdScanned: [],
             radarBlip: [],
             firstMove: fixedValues[i].firstMove,
-            goSurface:fixedValues[i].goSurface
+            goSurface: fixedValues[i].goSurface,
+            droneVisibility: fixedValues[i].droneVisibility,
+            visibleMonsters: new Map<number, creature>(),
         };
     }
 
@@ -125,7 +139,10 @@ while (true) {
             holdScanned: [],
             radarBlip: [],
             firstMove: undefined,
-            goSurface: undefined
+            goSurface: undefined,
+            visibleMonsters: undefined,
+            droneVisibility: undefined,
+
         };
     }
 
@@ -148,15 +165,34 @@ while (true) {
     for (let i = 0; i < visibleCreatureCount; i++) {
         var inputs: string[] = readline().split(' ');
         const creatureId: number = parseInt(inputs[0]);
-        creatures.forEach((element) => {
-            if (element.creatureId == creatureId) {
-                element.isVisible = true;
-                element.creatureX = parseInt(inputs[1]);
-                element.creatureY = parseInt(inputs[2]);
-                element.creatureVx = parseInt(inputs[3]);
-                element.creatureVy = parseInt(inputs[4]);
+        let element: creature = {};
+        if (fishies.has(creatureId)) {
+            element.isVisible = true;
+            element.creatureX = parseInt(inputs[1]);
+            element.creatureY = parseInt(inputs[2]);
+            element.creatureVx = parseInt(inputs[3]);
+            element.creatureVy = parseInt(inputs[4]);
+            fishies.set(creatureId, element);
+        }
+        else if (monsters.has(creatureId)) {
+            element.isVisible = true;
+            element.creatureX = parseInt(inputs[1]);
+            element.creatureY = parseInt(inputs[2]);
+            element.creatureVx = parseInt(inputs[3]);
+            element.creatureVy = parseInt(inputs[4]);
+            monsters.set(creatureId, element);
+            if (Math.abs(element.creatureX - player.drones[0].droneX) <= player.drones[0].droneVisibility &&
+                Math.abs(element.creatureY - player.drones[0].droneY) <= player.drones[0].droneVisibility) {
+                player.drones[0].visibleMonsters.set(element.creatureId, element);
+                console.error("goes for drone ID", player.drones[0].droneId);
             }
-        })
+            if (Math.abs(element.creatureX - player.drones[1].droneX) <= player.drones[1].droneVisibility &&
+                Math.abs(element.creatureY - player.drones[1].droneY) <= player.drones[1].droneVisibility) {
+                player.drones[1].visibleMonsters.set(element.creatureId, element);
+                console.error("goes for drone ID", player.drones[1].droneId);
+
+            }
+        }
     }
 
     const radarBlipCount: number = parseInt(readline());
@@ -186,7 +222,6 @@ while (true) {
                     light = 1;
                     fixedValues[i].goSurface = true;
                     fixedValues[i].firstMove = false;
-                    console.error(i, player.drones[i].goSurface, player.drones[i].firstMove);
                 }
             }
             else {
@@ -196,37 +231,55 @@ while (true) {
                     light = 1;
                     fixedValues[i].goSurface = true;
                     fixedValues[i].firstMove = false;
-                    console.error(i, player.drones[i].goSurface, player.drones[i].firstMove);
                 }
             }
-            console.error(i, player.drones[i].goSurface, player.drones[i].firstMove);
         }
-        else if (player.drones[i].goSurface == true)
-        {
+        else if (player.drones[i].goSurface == true) {
             x = player.drones[i].droneX;
             y = 500;
             if (player.drones[i].droneY == 500)
                 fixedValues[i].goSurface = false;
         }
-        
         else {
-            if (player.drones[i].droneY < 8500 && player.drones[i].droneX <= 3500) {
+
+            if (player.drones[i].droneY < 8500) {
                 y = 9000;
                 x = player.drones[i].droneX;
             }
-            else if (player.drones[i].droneY >= 8500 && player.drones[i].droneX < 8500) {
-                y = 9000;
-                x = 9000;
-            }
-            else if (player.drones[i].droneX >= 8500 && player.drones[i].droneY > 500) {
-                y = 500;
-                x = player.drones[i].droneX;
-            }
-            else {
-                y = 3500;
-                x = 1500;
+            else if (player.drones[i].droneY >= 8500) {
+               fixedValues[i].goSurface = true;
             }
         }
+        if (player.drones[i].firstMove == false && player.drones[i].droneY > 5000) {
+            fixedValues[i].countLight--;
+            if (fixedValues[i].countLight == 0) {
+                fixedValues[i].countLight = 3;
+                light = 1;
+            }
+        }
+        console.error(i, "light = ", fixedValues[i].countLight);
+
+        /*override previous instruction bcs you should avoid the big fishie*/
+        if (player.drones[i].visibleMonsters.size > 0) {
+            light = 0;
+            console.error("drone ID", player.drones[i].droneId, " should get moving");
+            player.drones[i].visibleMonsters.forEach((elem) => {
+                //  let distX = (abs)elem.creatureX;
+                if (player.drones[i].droneX >= elem.creatureX)
+                    x = 10000;
+                else if (player.drones[i].droneX < elem.creatureX)
+                    x = 0;
+                if (player.drones[i].droneY >= elem.creatureY)
+                    y = 10000;
+                else if (player.drones[i].droneY < elem.creatureY)
+                    y = 0;
+            })
+        }
+
+        if (light == 1)
+            fixedValues[i].droneVisibility = 2000;
+        else
+            fixedValues[i].droneVisibility = 800;
         console.log('MOVE', x, y, light);
     }
 }
